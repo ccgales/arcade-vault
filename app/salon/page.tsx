a171e66393ef4b94bd9f99ec825e6a11866dc5b1
Cristian Gales <gales98@hotmail.com>
2026-09-04 21:06:48 -0500

    commit-leaderboard-paso8

--- app/salon/page.tsx
+++ app/salon/page.tsx
@@ -1,5 +1,11 @@
 import HallOfFame from "@/components/HallOfFame";
+import { getGames } from "@/lib/games";
+import { getScoresByGame } from "@/lib/scores";
 
-export default function SalonPage() {
-  return <HallOfFame />;
+export default async function SalonPage() {
+  const games = await getGames();
+  const initialScores =
+    games.length > 0 ? await getScoresByGame(games[0].id) : [];
+
+  return <HallOfFame games={games} initialScores={initialScores} />;
 }
